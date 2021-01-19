68a122a49d1cdbf9a8889cca50de4cade443c0f3
stidsem4 <colin@cgoutte.fr>
2021-01-19 10:11:01 +0000

    ajout fichier affichage

---
+++ affichage.py
@@ -0,0 +1,13 @@
+print("Une phrase")
+
+nombre = 9.0
+
+print(nombre)
+carre  = nombre ** 2
+print(carre)
+
+print("le carré de", nombre, 'vaut', carre)
+
+message_carr = f"le carre de {nombre} faut {nombre**2}" 
+
+print(message_carr)
--- main.py
+++ main.py
@@ -9,8 +9,32 @@ from collections import namedtuple
 
 Point2D = namedtuple("Point2D", ['x' , 'y'])
 
-p0 = Point2D(0.5, 0.5)
+p0 = Point2D(0.5, 0.6)
 
-print(p0)
+def norme(p):
+  d2 = p[0] ** 2 + p[1] ** 2
+  return d2**0.5
+
+print(norme(point))
+print(norme((0,1)))
+
+
+point1 = (0,0)
+print(norme(point1))
+point2 = (1,1) 
+print(norme(point2))
+
+
+p0 = Point2D(1, 1)
+print(p0[0], p0[1])
+print(p0.x, p0.y)
+
+print(norme(p0))
+
+p1 = Point2D(1, 1)
+
+print(f"{p0} + {p1} = {p0+p1}")
+
+
 
 
