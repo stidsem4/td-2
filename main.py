8dad712ac7cfcf8d432e1c37f9106a8f7264227c
stidsem4 <colin@cgoutte.fr>
2021-01-19 09:53:12 +0000

    Point 2D namedtuple

--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 
 
 point  = (0.4, 0.5)
-
+#          x    y
 print(point)
+
+from collections import namedtuple
+
+Point2D = namedtuple("Point2D", ['x' , 'y'])
+
+p0 = Point2D(0.5, 0.5)
+
+print(p0)
+
 
