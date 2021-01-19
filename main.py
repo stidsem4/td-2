fba62c22b44aa1d98e59ebe922a3b57d746c9507
stidsem4 <colin@cgoutte.fr>
2021-01-19 10:18:09 +0000

    Creation de la fonction ajout point

--- main.py
+++ main.py
@@ -31,10 +31,18 @@ print(p0.x, p0.y)
 
 print(norme(p0))
 
-p1 = Point2D(1, 1)
+p1 = Point2D(0, 1)
 
 print(f"{p0} + {p1} = {p0+p1}")
 
+p3 = Point2D(1, 2)
 
+
+def ajout_point(p_1, p_2):
+  p3_x =p_1[0] + p_2[0]
+  p3_y =p_1[1] + p_2[1]
+  return Point2D(p3_x , p3_y)
+
+print(f"{p0} + {p1} = {ajout_point(p0,p1)}")
 
 
